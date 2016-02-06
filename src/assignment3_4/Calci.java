f862420c41e418baef6d962e26f8e06d80e3e9f9
sampathkumar11 <sampathkumar11@gmail.com>
2016-02-06 16:42:22 +0530

    Session 3-4
    Operator Checking not working. Help requested.

--- src/assignment3_4/Calci.java
+++ src/assignment3_4/Calci.java
@@ -1,7 +1,11 @@
 /**
+ if else check
  * 
  */
+
+
 package assignment3_4;
+import java.util.Scanner;
 
 public class Calci {
 	static String kalculate(Double fn,String opr,Double ln){
@@ -26,9 +30,24 @@ System.out.println("operator "+opr);
 		// TODO Auto-generated method stub
 	//	Scanner inputReader = new Scanner(System.in);
 		//System.out.println("Enter First Number :");
-		Double fn=Double.parseDouble(args[0]);
-		String opr=args[1];
-		Double ln=Double.parseDouble(args[2]);
+		Scanner scnr=new Scanner(System.in);
+		System.out.println("Enter First Number :");
+		String fns=scnr.nextLine();
+		Double fn=Double.parseDouble(fns);
+		
+		//Double fn=Double.parseDouble(args[0]);
+		
+		System.out.println("Enter the Arithmetic operator :\\n + for addition//n - for Subtraction//n * for Multiplication//n / for Division");
+		String opr=scnr.nextLine();
+		//Double fn=Double.parseDouble(fns);
+		
+		//String opr=args[1];
+		System.out.println("Enter Next Number :");
+		String lns=scnr.nextLine();
+		Double ln=Double.parseDouble(lns);
+		
+		
+		//Double ln=Double.parseDouble(args[2]);
 		String output=kalculate(fn, opr, ln);//"test output";
 		System.out.println(fn+" "+opr+" "+ln+" = "+output);
 	}
